c31f8d1394b021bffca64a29020956d3ccea9202
Maral <martinovalexei568@mail.com>
2024-12-05 16:37:54 +0300

    Дополнительные задания

--- dodzadachi.py
+++ dodzadachi.py
@@ -4,3 +4,6 @@
 # models_data = {"BMW": 1, "ВАЗ": 2, "Tesla": ["Modes S", "WV", "Tiggo"]}
 # print(models_data.get ("Tesla")[0])
 
+d1 = {"a": 100, "b": 200, "c": 300}
+d2 = {"a": 300, "b": 200, "d": 300}
+print(d1["b"] == d2["b"])
