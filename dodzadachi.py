b337864a1b50d845635206626623ec4b25fb3b8f
Maral <martinovalexei568@mail.com>
2024-12-05 16:27:00 +0300

    Дополнительные задания

---
+++ dodzadachi.py
@@ -0,0 +1,6 @@
+# person = {"name": "Kelly", "age":25, "city": "New york"}
+# print (person.get("age"))
+
+# models_data = {"BMW": 1, "ВАЗ": 2, "Tesla": ["Modes S", "WV", "Tiggo"]}
+# print(models_data.get ("Tesla")[0])
+
