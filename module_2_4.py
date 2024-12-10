2252e96e99bbf27e6b24451189442004982d23a8
Maral <martinovalexei568@mail.com>
2024-12-10 07:56:22 +0300

    Zadanie module_2_4

--- module_2_4.py
+++ module_2_4.py
@@ -1,3 +1,24 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-primes = {}
-not_primes = {}
+n = 0
+primes = []
+not_primes = []
+i = 0
+for i in range(len(numbers)):
+    is_prime = True
+    n = numbers[i]
+    if n < 2:  # не 0 и не 1
+        print(n, '- не простое и не сложное число')
+        continue
+    else:
+        f = n ** (1 / 2)
+    for a in range(2, int(f + 1)):
+        if n % a == 0:
+            is_prime = False
+            break
+    if not (is_prime):
+        not_primes.append(n)
+    else:
+        primes.append(n)
+is_prime = True
+print('Primes: ', primes)
+print('Not Primes: ', not_primes)
