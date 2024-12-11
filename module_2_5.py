f3203e147c7204d08d55319e3ea0cc9264219cc9
Maral <martinovalexei568@mail.com>
2024-12-11 12:56:59 +0300

    Zadanie module_2_5

--- module_2_5.py
+++ module_2_5.py
@@ -9,5 +9,4 @@ def get_matrix(n, m, value):
 n = int(input('Введите число :'))
 m = int(input('Введите число :'))
 value = input(f'Введите число : ')
-print('-------' * m)
 matrix = get_matrix(n, m, value)
